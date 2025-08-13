1b452aa18a09d889eb0b89476134b39517ba11e0
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2025-08-13 23:34:39 +0530

    Contract Generated

--- packages/nextjs/app/debug/_components/DebugContracts.tsx
+++ packages/nextjs/app/debug/_components/DebugContracts.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 import { IVendingMachine } from "./IVendingMachine";
 import { ethers } from "ethers";
 
-const contractAddress = "0x4f6e1bc44ee6f1dd0951b8e5bde4764055918055"; // Get this from run-dev-node.sh output
+const contractAddress="0x03af12b080931f9d141297e561c65386ed4d1a13"; // Get this from run-dev-node.sh output
 const provider = new ethers.JsonRpcProvider(process.env.NEXT_PUBLIC_RPC_URL || "");
 const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || "";
 const signer = new ethers.Wallet(privateKey, provider);
